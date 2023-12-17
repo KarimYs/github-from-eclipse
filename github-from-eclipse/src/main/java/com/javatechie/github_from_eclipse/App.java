2db9c9dbea841b89b96021d21c7a78140e88b821
KarimYs <yskarim9154@gmail.com>
2023-12-17 11:18:33 +0530

    First commit

---
+++ github-from-eclipse/src/main/java/com/javatechie/github_from_eclipse/App.java
@@ -0,0 +1,8 @@
+package com.javatechie.github_from_eclipse;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
